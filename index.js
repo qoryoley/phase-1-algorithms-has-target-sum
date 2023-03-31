de2b416809c6be0750334d932da83c9e102b5e4e
Abdalla <qoryoley@gmail.com>
2023-03-31 11:19:56 +0300

    Algorithm has target sum

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i <  array.length; i++){
+    // n steps
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: 0(n2)
+  space: o
 */
 
 /* 
